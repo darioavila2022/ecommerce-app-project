f8553b13e83f1a41a5e0b449b88c768e0fc70ffa
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2023-01-24 15:37:08 -0300

    Account page elements added

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 import Ads from '../components/Ads.js';
 import Navbar from '../components/Navbar.js';
 import Footer from '../components/Footer.js';
@@ -18,30 +16,55 @@ const Profile = () => {
                             <h2>NAME LASTNAME</h2>
                             <h2>email@email.com</h2>
                         </div>
-                        <ul className='acct-links'>
-                            <li><Link to='#' className='link'>Account</Link></li>
-                            <li><Link to='#' className='link'>Password</Link></li>
-                            <li><Link to='#' className='link'>My Orders</Link></li>
-                            <li><Link to='#' className='link'>Payments Info</Link></li>
-                            <li><Link to='#' className='link'>Delete Account</Link></li>
-                            <li><Link to='#' className='link'>Logout</Link></li>
-                        </ul>
+                        <div className='acct-elements'>
+                            <div><button>LOG OUT</button></div>
+                        </div>
                     </div>
 
                     <div className='acct-right'>
-                        <div>
-                            <h2>Account</h2>
-                            <input className='acct-input' placeholder="name" />
-                            <input className='acct-input' placeholder="last name" />
-                            <input className='acct-input' placeholder="email" />
-                            <input className='acct-input' placeholder="username" />
-                            <input className='acct-input' placeholder="country" />
-                            <input className='acct-input' placeholder="city" />
-                            <input className='acct-input' placeholder="address" />
-                            <input className='acct-input' placeholder="phone" />
+                        <div className='acct-elements'>
+                            <div>
+                                <h2>Account</h2>
+                                <input className='acct-input' placeholder="name" />
+                                <input className='acct-input' placeholder="last name" />
+                                <input className='acct-input' placeholder="email" />
+                                <input className='acct-input' placeholder="username" />
+                                <input className='acct-input' placeholder="country" />
+                                <input className='acct-input' placeholder="city" />
+                                <input className='acct-input' placeholder="address" />
+                                <input className='acct-input' placeholder="phone" />
+                            </div>
+                            <div><button>UPDATE</button></div>
                         </div>
-                        <div>
-                            <button>UPDATE</button>
+
+                        <div className='acct-elements'>
+                            <div>
+                                <h2>Orders</h2>
+                                <input className='acct-input' placeholder="Order #1" />
+                                <input className='acct-input' placeholder="21/05/2000" />
+                                <input className='acct-input' placeholder="Order #2" />
+                                <input className='acct-input' placeholder="09/07/3000" />
+                                <input className='acct-input' placeholder="Order #3" />
+                                <input className='acct-input' placeholder="15/09/1999" />
+                            </div>
+                        </div>
+
+                        <div className='acct-elements'>
+                            <div>
+                                <h2>Password</h2>
+                                <input className='acct-input' placeholder="password" />
+                                <input className='acct-input' placeholder="new password" />
+                            </div>
+                            <div><button>UPDATE</button></div>
+                        </div>
+
+                        <div className='acct-elements'>
+                            <div>
+                                <h2>Payment</h2>
+                                <input className='acct-input' placeholder="credit card number" />
+                                <input className='acct-input' placeholder="expiration date" />
+                            </div>
+                            <div><button>UPDATE</button></div>
                         </div>
                     </div>
                 </div>
