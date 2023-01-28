01ebf8fc9bc66303543a07d55b6093fa12ea1613
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2023-01-28 11:17:41 -0300

    Register section mechanics

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,60 @@
+// import Navbar from '../components/Navbar.js';
+// import Ads from '../components/Ads.js';
+// import Footer from '../components/Footer.js';
+// import './login.css'
+// import { useState } from 'react';
+// import { loginFailure } from '../redux/userRedux.js';
+// import { login } from '../redux/apiCalls.js';
+// import { useDispatch, useSelector } from 'react-redux';
+
+// const Login = () => {
+//     const [username, setUserName] = useState("")
+//     const [password, setPassword] = useState("")
+//     const dispatch = useDispatch()
+//     const { error } = useSelector((state) => state.user);
+
+//     const handleClick = (e) => {
+//         e.preventDefault()
+//         login(dispatch, { username, password })
+//     }
+//     return (
+//         <div>
+//             <Ads />
+//             <Navbar />
+//             <div className="login-container">
+//                 <div className="login-wrapper">
+//                     <h1>SIGN IN</h1>
+//                     <form className='login-form'>
+//                         <input
+//                             className='login-input'
+//                             placeholder="username"
+//                             onChange={(e) => setUserName(e.target.value)}
+//                         />
+//                         <input
+//                             className='login-input'
+//                             type="password"
+//                             placeholder="password"
+//                             onChange={(e) => setPassword(e.target.value)}
+//                         />
+//                         <button
+//                             className='login-btn'
+//                             onClick={handleClick}
+//                         >Login</button>
+
+//                         {error && <span className='error'>Hm, something went wrong. Check your spelling and try again, please.</span>}
+
+//                         <a className="login-link">Forgot your password?</a>
+//                         <a className="login-link">Create a new account</a>
+//                     </form>
+//                 </div>
+//             </div>
+//             <Footer />
+//         </div>
+//     )
+// };
+
+// export default Login
+
 import Navbar from '../components/Navbar.js';
 import Ads from '../components/Ads.js';
 import Footer from '../components/Footer.js';
@@ -8,14 +65,14 @@ import { login } from '../redux/apiCalls.js';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Login = () => {
-    const [username, setUserName] = useState("")
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const dispatch = useDispatch()
     const { error } = useSelector((state) => state.user);
 
     const handleClick = (e) => {
         e.preventDefault()
-        login(dispatch, { username, password })
+        login(dispatch, {email, password})
     }
     return (
         <div>
@@ -25,20 +82,21 @@ const Login = () => {
                 <div className="login-wrapper">
                     <h1>SIGN IN</h1>
                     <form className='login-form'>
-                        <input
-                            className='login-input'
-                            placeholder="username"
-                            onChange={(e) => setUserName(e.target.value)}
+                        <input 
+                        className='login-input' 
+                        placeholder="email" 
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)} 
                         />
-                        <input
-                            className='login-input'
-                            type="password"
-                            placeholder="password"
-                            onChange={(e) => setPassword(e.target.value)}
+                        <input 
+                        className='login-input' 
+                        type="password"
+                        placeholder="password" 
+                        onChange={(e) => setPassword(e.target.value)} 
                         />
-                        <button
-                            className='login-btn'
-                            onClick={handleClick}
+                        <button 
+                        className='login-btn'
+                        onClick={handleClick}
                         >Login</button>
 
                         {error && <span className='error'>Hm, something went wrong. Check your spelling and try again, please.</span>}
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,31 +1,105 @@
-import Ads from '../components/Ads.js';
-import Footer from '../components/Footer.js';
-import Navbar from '../components/Navbar.js';
-import './register.css';
+// import Ads from '../components/Ads.js';
+// import Footer from '../components/Footer.js';
+// import Navbar from '../components/Navbar.js';
+// import './register.css';
 
-const Register = () => {
+// const Register = () => {
+//     return (
+//         <div>
+//             <Ads />
+//             <Navbar />
+//             <div className="reg-container">
+//                 <div className="reg-wrapper">
+//                     <h1>CREATE YOUR ACCOUNT</h1>
+//                     <form className='reg-form'>
+//                         <input className='reg-input' placeholder="name" />
+//                         <input className='reg-input' placeholder="last name" />
+//                         <input className='reg-input' placeholder="email" />
+//                         <input className='reg-input' placeholder="username" />
+//                         <input className='reg-input' placeholder="password" />
+//                         <input className='reg-input' placeholder="confirm password" />
+//                         <div className="reg-terms">I hereby agree to the storage of my own personal information, as well as other information not expressed here that may be suitable for this webpage's sponsors; in accordance with the <b>POLICIES</b>.</div>
+//                         <button className='reg-btn'>SEND</button>
+//                     </form>
+//                 </div>
+//             </div>
+//             <Footer />
+//         </div>
+//     )
+// };
+
+// export default Register;
+
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Register() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [register, setRegister] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const configuration = {
+            method: "post",
+            url: "http://localhost:5000/api/auth/register",
+            data: {
+                email,
+                password,
+            },
+        };
+
+        axios(configuration)
+    .then((result) => {setRegister(true)})
+    .catch((error) => {error= new Error()})
+    };
+
+
+    
     return (
-        <div>
-            <Ads />
-            <Navbar />
-            <div className="reg-container">
-                <div className="reg-wrapper">
-                    <h1>CREATE YOUR ACCOUNT</h1>
-                    <form className='reg-form'>
-                        <input className='reg-input' placeholder="name" />
-                        <input className='reg-input' placeholder="last name" />
-                        <input className='reg-input' placeholder="email" />
-                        <input className='reg-input' placeholder="username" />
-                        <input className='reg-input' placeholder="password" />
-                        <input className='reg-input' placeholder="confirm password" />
-                        <div className="reg-terms">I hereby agree to the storage of my own personal information, as well as other information not expressed here that may be suitable for this webpage's sponsors; in accordance with the <b>POLICIES</b>.</div>
-                        <button className='reg-btn'>SEND</button>
-                    </form>
+        <>
+            <h2>Register</h2>
+            <form onSubmit={(e) => handleSubmit(e)}>
+ 
+                <div >
+                    <div>Email</div>
+                    <input
+                        className='reg-input'
+                        placeholder="email"
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
-            </div>
-            <Footer />
-        </div>
-    )
-};
 
-export default Register;
+                <div >
+                    <div>Password</div>
+                    <input
+                        className='reg-input'
+                        placeholder="password"
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                <button
+                    variant="primary"
+                    type="submit"
+                    onClick={(e) => handleSubmit(e)}
+                >
+                    Register
+                </button>
+
+                {register ? (
+                    <p className="text-success">You Are Registered Successfully</p>
+                ) : (
+                    <p className="text-danger">You Are Not Registered</p>
+                )}
+            </form>
+        </>
+    );
+}
