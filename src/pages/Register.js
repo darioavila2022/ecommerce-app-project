c9842c5b521796fdb10617104c656315fcf1ffaa
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2023-01-29 18:46:10 -0300

    HTTPS change

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -19,7 +19,7 @@ export default function Register() {
 
         const configuration = {
             method: "post",
-            url: "http://ecommerce.app.up.railway.app/api/auth/register",
+            url: "https://ecommerce.app.up.railway.app/api/auth/register",
             data: {
                 email,
                 password,
