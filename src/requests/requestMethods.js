da615a1216f78e8500b41fb42605b0be59aeb2c7
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2023-01-30 02:00:18 -0300

    Links update

--- src/requests/requestMethods.js
+++ src/requests/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://ecommerce-app.up.railway.app/";
+const BASE_URL = "https://ecommerce-app.up.railway.app/api/";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
